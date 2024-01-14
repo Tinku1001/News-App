33301398e3fa19e93630f0c82b7b199965533c3a
Tinku1001 <tinkukumar10012001@gmail.com>
2024-01-14 10:18:35 +0530

    newsfree

--- src/App.js
+++ src/App.js
@@ -1,24 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+const App =()=> {
+ const pageSize = 10;
+ const apiKey='cca3f68b073e4aa9a6a0e55ecb4ef453'
+
+  const [progress, setProgress] = useState(0)
+
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     mode: "light",
+  //     alert: null,
+  //   };
+  // }
+
+  // showAlert = (message, type) => {
+  //   this.setState({
+  //     alert: {
+  //       msg: message,
+  //       type: type,
+  //     }
+  //   });
+
+  //   setTimeout(() => {
+  //     this.setState({
+  //       alert: null
+  //     });
+  //   }, 1500);
+  // };
+
+  // toggleMode = () => {
+  //   if (this.state.mode === "light") {
+  //     this.setState({ mode: "dark" });
+  //     document.body.style.backgroundColor = "#042743";
+  //     this.showAlert("Dark mode has been enabled", "success");
+  //   } else {
+  //     this.setState({ mode: "light" });
+  //     document.body.style.backgroundColor = "white";
+  //     this.showAlert("Light mode has been enabled", "success");
+  //   }
+  // };
+
+    return (
+      <div>
+        <Router>
+          <Navbar />
+
+          <LoadingBar color="#f11946" progress={progress} />
+
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <News setProgress ={setProgress} apiKey={apiKey}
+                  key="general"
+                  pageSize={pageSize}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+
+            <Route
+              exact
+              path="/business"
+              element={
+                <News setProgress={setProgress} 
+                  key="business"
+                  pageSize={pageSize}
+                  country="in"
+                  category="business"
+                />
+              }
+            ></Route>
+            <Route
+              path="/entertainment"
+              element={
+                <News setProgress={setProgress} 
+                  key="entertainment"
+                  pageSize={pageSize}
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            ></Route>
+
+            {/*
+            <Route
+              exact
+              path="/general"
+              element={
+                <News setProgress={setProgress} 
+                  key="general"
+                  pageSize={pageSize}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            */}
+
+            <Route
+              exact
+              path="/health"
+              element={
+                <News setProgress={setProgress} 
+                  key="health"
+                  pageSize={pageSize}
+                  country="in"
+                  category="health"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/science"
+              element={
+                <News setProgress={setProgress} 
+                  key="science"
+                  pageSize={pageSize}
+                  country="in"
+                  category="science"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News setProgress={setProgress} 
+                  key="sports"
+                  pageSize={pageSize}
+                  country="in"
+                  category="sports"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News setProgress={setProgress} 
+                  key="technology"
+                  pageSize={pageSize}
+                  country="in"
+                  category="technology"
+                />
+              }
+            ></Route>
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
+  export default App;
---
+++ src/components/News.js
@@ -0,0 +1,175 @@
+import React, { useEffect,useState } from "react";
+
+import NewsItem from "./NewsItem";
+import Spinner from "./spinner";
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
+const News=(props)=> {
+
+ const [articles, setArticles] = useState([])
+ const [loading, setLoading] = useState(true)
+ const [page, setPage] = useState(1)
+ const [totalResults, setTotalResults] = useState(0)
+
+  const capitalizeFirstLetter=(string)=> {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+   
+  
+
+  const updateNews=async()=>{
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=cca3f68b073e4aa9a6a0e55ecb4ef453&page=${page}&pageSize=${props.pageSize}`;
+
+   
+    setLoading(true)
+    let data = await fetch(url);
+    props.setProgress(40);
+    let parsedData = await data.json();
+
+    setArticles(parsedData.articles)
+    setTotalResults(parsedData.totalResults)
+    setLoading(false)
+    
+    props.setProgress(100);
+
+  }
+  useEffect(() => {
+    document.title=`${capitalizeFirstLetter(props.category)}-NewsMonkey`
+
+    updateNews();
+    // eslint-disable-next-line
+  }, [page])
+  
+  // const componentDidMount=async()=> {
+    // let url = `https://newsapiorg/v2/top-headlines?country=${propscountry}&category=${propscategory}&apiKey=cca3f68b073e4aa9a6a0e55ecb4ef453&page=1&pageSize=${propspageSize}`;
+    // setState({ loading: true });
+    // let data = await fetch(url);
+    // let parsedData = await datajson();
+    // setState({
+    //   articles: parsedDataarticles,
+    //   totalResults: parsedDatatotalResults,
+    //   loading: false,
+    // });
+  //   updateNews();
+  // }
+  
+
+  // const handleNextClick = async () => {
+    // if (
+    //   !(statepage + 1 >
+    //   Mathceil(statetotalResults / propspageSize))
+    // ) {
+    //   let url = `https://newsapiorg/v2/top-headlines?country=${propscountry}&category=${propscategory}&apiKey=cca3f68b073e4aa9a6a0e55ecb4ef453&page=${
+    //     statepage + 1
+    //   }&pageSize=${propspageSize}`;
+
+    //   setState({ loading: true });
+    //   let data = await fetch(url);
+    //   let parsedData = await datajson();
+    //   setState({
+    //     page: statepage + 1,
+    //     articles: parsedDataarticles,
+    //     totalResults: parsedDatatotalResults,
+    //     loading: false,
+    //   });
+    // }
+
+  
+  //   setPage(page+1)
+  //   updateNews();
+  // };
+  // const handlePrevClick = async () => {
+    // let url = `https://newsapiorg/v2/top-headlines?country=${propscountry}&category=${propscategory}&apiKey=cca3f68b073e4aa9a6a0e55ecb4ef453&page= ${
+    //   statepage - 1
+    // }&pageSize=${propspageSize}`;
+    // setState({ loading: true });
+    // let data = await fetch(url);
+    // let parsedData = await datajson();
+    // setState({
+    //   page: statepage - 1,
+    //   articles: parsedDataarticles,
+    //   loading: false,
+    // });
+
+    
+  //   setPage(page-1)
+  //   updateNews();
+  // };
+
+  const fetchMoreData = async() => {
+    // a fake async api call like which sends
+    // 20 more records in 15 secs
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=cca3f68b073e4aa9a6a0e55ecb4ef453&page=${page}&pageSize=${props.pageSize}`;
+
+    setPage(page+1) 
+    
+     
+  
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles))
+    setTotalResults(parsedData.totalResults)
+
+  };
+
+    return (
+      <>
+        <h1 className="text-center" style={{margin:'30px',marginTop:'80px'}}>NewsMonkey-Top {capitalizeFirstLetter(props.category)} headlines</h1>
+  
+  {loading && <Spinner />}
+  
+  
+  <InfiniteScroll
+  dataLength={articles.length}
+  next={fetchMoreData}
+  hasMore={articles.length!==totalResults}
+  loader={<Spinner/>}
+>
+<div className="container">
+        <div className="row">
+          {/*{!stateloading &&*/}
+            {articles.map((element) => {
+              return (
+                <div className="col-md-4 my-3" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 35) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 60)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}
+                  />
+                </div>
+                
+              );
+            })}
+        </div>
+        </div>
+        
+        </InfiniteScroll>
+        
+       
+      </>
+    );
+  }
+
+
+News.defaultProps={
+  country:'in',
+  pageSize:6,
+  category:'general'
+ 
+
+}
+News.propTypes={
+  country:PropTypes.string,
+  pageSize:PropTypes.number,
+  category:PropTypes.string,
+}
+
+export default News;
